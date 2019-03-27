7680440065f5bdd0b0b98548cc8e6982c5b6c91f
sabbasi <sabbasi@crkrl-abbassha1.corp.contoursoftware.net>
2019-03-28 04:00:24 +0500

    1. voucher form is added 2. print is in progress

---
+++ src/main/java/com/web/dto/education/FeeVoucherDTO.java
@@ -0,0 +1,107 @@
+package com.web.dto.education;
+
+import java.time.LocalDate;
+
+/**
+ * 
+ */
+
+/**
+ * @author sabbasi
+ *
+ */
+public class FeeVoucherDTO {
+
+	private Short vb = 1;
+	private String vi = null;
+	private Short vp = 1;
+	private String sdStr = null;
+	private String edStr = null;
+	private LocalDate sd = LocalDate.now();
+	private LocalDate ed = LocalDate.now();
+	/**
+	 * @return the vb
+	 */
+	public Short getVb() {
+		return vb;
+	}
+	/**
+	 * @param vb the vb to set
+	 */
+	public void setVb(Short vb) {
+		this.vb = vb;
+	}
+	/**
+	 * @return the vi
+	 */
+	public String getVi() {
+		return vi;
+	}
+	/**
+	 * @param vi the vi to set
+	 */
+	public void setVi(String vi) {
+		this.vi = vi;
+	}
+	/**
+	 * @return the vp
+	 */
+	public Short getVp() {
+		return vp;
+	}
+	/**
+	 * @param vp the vp to set
+	 */
+	public void setVp(Short vp) {
+		this.vp = vp;
+	}
+	/**
+	 * @return the sdStr
+	 */
+	public String getSdStr() {
+		return sdStr;
+	}
+	/**
+	 * @param sdStr the sdStr to set
+	 */
+	public void setSdStr(String sdStr) {
+		this.sdStr = sdStr;
+	}
+	/**
+	 * @return the edStr
+	 */
+	public String getEdStr() {
+		return edStr;
+	}
+	/**
+	 * @param edStr the edStr to set
+	 */
+	public void setEdStr(String edStr) {
+		this.edStr = edStr;
+	}
+	/**
+	 * @return the sd
+	 */
+	public LocalDate getSd() {
+		return sd;
+	}
+	/**
+	 * @param sd the sd to set
+	 */
+	public void setSd(LocalDate sd) {
+		this.sd = sd;
+	}
+	/**
+	 * @return the ed
+	 */
+	public LocalDate getEd() {
+		return ed;
+	}
+	/**
+	 * @param ed the ed to set
+	 */
+	public void setEd(LocalDate ed) {
+		this.ed = ed;
+	}
+	
+}
